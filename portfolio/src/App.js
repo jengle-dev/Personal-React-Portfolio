11ef3fa492a0218eba7e36393ad75062c7521b78
jengle-dev <j.engle.dev@gmail.com>
2023-04-28 18:31:03 -0500

    initial commit of file structure etc.

---
+++ portfolio/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      {/* <Navigation />
+      <Portfolio />
+      <Footer /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/Header.js
@@ -0,0 +1,13 @@
+//Header
+import React from 'react';
+
+function Header() {
+    return(
+        //build the html
+        <header>
+        <h1>Jennifer Engle</h1>
+        </header>
+    )
+}
+
+export default Header;
