2f9dff5d39fae815c7bb9675a9a05e638d520735
jengle-dev <j.engle.dev@gmail.com>
2023-05-06 18:34:28 -0500

    fixed icon links in footer

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -2,7 +2,6 @@ import React from "react"
 import gitHub from "../assets/github-square-white.png"
 import linkedIn from "../assets/linkedin-white-only.png" 
 import insta from "../assets/Instagram.png"
-import {Link} from "react-router-dom" 
 // need to install the above package
 
 const Footer = () => {
@@ -11,9 +10,9 @@ const Footer = () => {
     <p>Jennifer Engle  |  ©2023  |
       {/* react component with link to = */}
     {/* <Link to={"t"}><img className="icon" src={gitHub} alt="github icon" /></Link> |   */}
-    <img className="icon" src={linkedIn} alt="linkedin icon" /> |    
-    <img className="icon" src={insta} alt="instagram icon"/>  |    
-    <img className="icon" src={gitHub} alt="github icon" />
+    <a href="https://linkedin.com/in/jennifer-engle" target="_blank"><img className="icon" src={linkedIn} alt="linkedin icon" /> </a>  |  
+    <a href="https://instagram.com/jengle2984/" target="_blank"><img className="icon" src={insta} alt="instagram icon"/></a>  |  
+    <a href="https://github.com/jengle-dev" target="_blank"><img className="icon" src={gitHub} alt="github icon" /></a>
     </p>
     <p className="icon"></p>
   </footer>  )
